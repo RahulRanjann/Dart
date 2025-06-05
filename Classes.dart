3797fd30930a68b6bb660f98bb2c95faaec93993
rahulranjann <rahul1913111054@gmail.com>
2025-06-05 16:26:45 +0530

    Enhance Cookie class with private variable accessors and update main function to demonstrate usage

--- Classes.dart
+++ Classes.dart
@@ -6,12 +6,30 @@ void main() {
   print(cookie.size);
   cookie.baking();
   cookie.isCooling();
+  // printing the private variable
+  print(cookie.height);
+  // setting the private value using setter
+  cookie.setHeight = 12;
+  print(cookie.height);
+
 }
 
 class Cookie {
   Cookie() {
     print("This line is prined by constructor!");
   }
+  // Private  Variables
+  int _height = 5;
+  int _weidth = 4;
+
+  // we use geater and seater to print and modify private variables.
+
+  get height => _height;
+
+  set setHeight(int h) {
+    _height = h;
+  }
+
   String shape = "Circle";
   double size = 12.05;
 
